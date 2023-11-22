92e4a214e4ff05a61a190cf77cfbc6e7e82c30cf
holickka <64195032+holickka@users.noreply.github.com>
2023-11-22 21:14:30 +0800

    Add files via upload
    updated with create read write open close examples

--- get_next_line.c
+++ get_next_line.c
@@ -1,20 +1,24 @@
 #include "get_next_line.h"
-/*
+
 char	*get_next_line(int fd)
 {
 	static char	*buffer;
-	char		*dest;
 	static int	i;
+	char		*dest;
 	int			a;
 
 	i = 0;
 	a = 0;
+	/* create buffer to store read data */
 	buffer = (char *)ft_calloc(BUFFER_SIZE, sizeof(char));
 	if (!buffer)
 		return (NULL);
 	bytesread = read(fd, buffer, BUFFER_SIZE);
 	if (bytesread > 0)
 	{
+		if (buffer[i] != '\n')
+			i++;
+			count++;
 		dest = (char *)ft_calloc(ft_strlen(buffer), sizeof(char));
 		if (!dest)
 			return (NULL);
@@ -25,20 +29,48 @@ char	*get_next_line(int fd)
 	}
 	return (NULL);
 }
-*/
-int	test()
-{
-	static int	*a;
-	a = 0;
-	a++;
-	return (a);
-}
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
 int	main()
 {
-	printf("%d", test());
-	printf("%d", test());
+	int		fd;
+	int		byteswrite;
+	int		bytesread;
+	char	buf[100];
+	char	data[] = "Hello\nWorld";
+	
+	/*create a new file*/
+	fd = open("file.txt", O_RDWR | O_CREAT, 0666);
+	if (fd == -1)
+		return (0);
 
+	/*write something into file*/
+	byteswrite = write(fd, data, strlen(data));
+	if (byteswrite == -1)
+		return (0);
+	
+	/*close it to reset pointer after write*/
+	close(fd);
+
+	/*open it again*/
+	fd = open("file.txt", O_RDWR);
+
+	/*try to read it*/
+	bytesread = read(fd, buf, byteswrite);
+	if (bytesread == -1 )
+		return (0);
+	buf[bytesread] = '\0';
+
+	/*print it out*/
+	printf("%s\n", buf);
+
+//	while (get_next_line())
+//		printf("%s\n", get_next_line());
 }
+
 /* read content from file to buffer */
 
 
@@ -48,6 +80,7 @@ int	main()
 to read text file pointed by file descriptor,
 one line at a time
 malloc and returns the line that's read
+returned line should include \n
 if nothing to read or error, return NULL
 
 | about read() |
---
+++ main1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/* usecase example of 'create read write open close' */
+
+int	main()
+{
+	int		fd;
+	int		byteswrite;
+	int		bytesread;
+	char	buf[100];
+	char	data[] = "Hello\nWorld";
+
+	/*create a new file*/
+	fd = open("file.txt", O_RDWR | O_CREAT, 0666);
+	if (fd == -1)
+		return (0);
+
+	/*write something into file*/
+	byteswrite = write(fd, data, strlen(data));
+	if (byteswrite == -1)
+		return (0);
+	
+	/*close it to reset pointer after write*/
+	close(fd);
+
+	/*open it again*/
+	fd = open("file.txt", O_RDWR);
+
+	/*try to read it*/
+	bytesread = read(fd, buf, byteswrite);
+	if (bytesread == -1 )
+		return (0);
+	buf[bytesread] = '\0';
+	
+	/*print it out*/
+	printf("%s\n", buf);
+
+//	while (get_next_line())
+//		printf("%s\n", get_next_line());
+}
