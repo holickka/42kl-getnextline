0d2343156f88ee5517d1f34a541e106b5372c244
holickka <64195032+holickka@users.noreply.github.com>
2023-12-11 20:37:06 +0800

    updated with norminette

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: hsim <hsim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 17:54:48 by hsim              #+#    #+#             */
-/*   Updated: 2023/12/11 20:07:22 by hsim             ###   ########.fr       */
+/*   Updated: 2023/12/11 20:36:36 by hsim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,10 +31,7 @@ typedef struct s_list
 char	*get_next_line(int fd);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 t_list	*ft_lstnew(void *content);
-int		ft_lstsize(t_list *lst);
 void	ft_lstclear(t_list **lst);
-void	*ft_calloc(size_t count, size_t n);
-size_t	ft_strlen(const char *s);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 
